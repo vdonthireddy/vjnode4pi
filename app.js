ed7d509d39213e629689bc798e494462fb232566
Vijay Donthireddy <vdonthireddy@SC-DonthireddyV.local>
2019-10-31 14:14:21 -0700

    created index.html

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 
 const express = require('express');
 const request = require('request');
+const fs = require('fs');
+
 var app = express();
 
 
 app.get("/", (req, res) => {
-    res.send("Hello Docker!!! - Vijay\n");
+    res.send("Hello Docker!!! - Vijay.");
+});
+
+app.get("/index", (req, res) => {
+    fs.readFile("index.html", function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+    });
 });
 
 app.listen(8080, '0.0.0.0');
