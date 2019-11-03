56775754b0ef2ee877d241a07dc7704aa4d065ff
Vijaya Donthireddy <donmac@vijayas-mbp.lan>
2019-11-03 09:16:53 -0800

    code changed

--- allrun.sh
+++ allrun.sh
@@ -4,6 +4,6 @@ git commit -m 'code changed'
 git push
 docker rmi -f vjnode4pi
 docker build -t vjnode4pi .
-docker tag vjnode4pi vdonthireddy/vjnode4pi:5.0
-docker push vdonthireddy/vjnode4pi:5.0
-docker run -p:8081:8080 -d vdonthireddy/vjnode4pi:5.0
+docker tag vjnode4pi vdonthireddy/vjnode4pi:6.0
+docker push vdonthireddy/vjnode4pi:6.0
+docker run -p:8081:8080 -d vdonthireddy/vjnode4pi:6.0
